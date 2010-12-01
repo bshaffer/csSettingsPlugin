5d82d3608fa4d936722169fe85c8484b01bc68c8
Oli Kessler <ok@Sting.local>
2010-12-01 13:22:42 +0100

    added authentication check on setting name link

--- modules/csSetting/templates/_list_td_tabular.php
+++ modules/csSetting/templates/_list_td_tabular.php
@@ -1,5 +1,9 @@
 <td class="sf_admin_text sf_admin_list_td_name">
-  <?php echo link_to($cs_setting->getName(), 'cs_setting_edit', $cs_setting) ?>
+  <?php if (csSettings::isAuthenticated($sf_user)): ?>
+    <?php echo link_to($cs_setting->getName(), 'cs_setting_edit', $cs_setting) ?>
+  <?php else: ?>
+    <?php echo $cs_setting->getName() ?>
+  <?php endif ?>
 </td>
 <td class="sf_admin_text sf_admin_list_td_value">
   <?php if ($form[$cs_setting['slug']]->hasError()): ?>
